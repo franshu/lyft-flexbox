91801e75090f122626c3b2612f873d5692d98acd
franshu <fran.ojeda208@gmail.com>
2017-06-05 03:43:39 -0300

    avance

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,28 @@
-javascript
+//EFECTO SCROLL DEL NAV
+/* Código de Liliana Kastilio: https://codepen.io/lili2311/pen/dJjuL< */
+window.addEventListener('scroll', function () {
+	const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+	shrinkOn = 100,
+	navElem = document.getElementById('navbar');
+	signUp = document.getElementById('signup-btn');
+	logo = document.getElementById('logonavbar');
+
+	if (distanceY > shrinkOn) {
+		navElem.classList.add("nav-color");
+		signUp.classList.add("show-signup-btn");
+		logo.src = "assets/images/logo-pink.png";
+	} else {
+		navElem.classList.remove("nav-color");
+		signUp.classList.remove("show-signup-btn");
+		logo.src = "assets/images/logo-white.png";
+	}
+});
+/*valida numero phone */
+
+document.getElementById('validarphone').addEventListener('click', function(){
+	var input = Array.from(document.getElementsByClassName('input'));
+	input.forEach(function(el){
+		el.classList.remove('hidden');
+	});
+});
+
